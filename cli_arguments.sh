1b84ede3f015a3195483386283dc8377bddd5231
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-17 09:08:29 +0000

    ds

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Script qui demande à l'utilisateur d'entrer des arguments et les affiche ligne par ligne.
-echo "Entrez plusieurs arguments séparés par des espaces :"
 read -a arg  
 compte=1
 for val in "${arg[@]}"; do
