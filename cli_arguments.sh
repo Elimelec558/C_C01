8ebde74a4573d6c48fbff75718de22cf09acbf7c
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-17 13:12:44 +0000

    fs

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-# Affiche chaque argument ligne par ligne
+
+# Vérifier si des arguments ont été fournis
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 argument1 argument2 ..."
+    exit 1
+fi
+
+# Utilisation de $@ pour parcourir les arguments
+echo "Affichage des arguments avec \$@ :"
 for arg in "$@"; do
-  echo "$arg"
+    echo "$arg"
+done
+
+echo ""
+
+# Utilisation de shift pour parcourir les arguments un par un
+echo "Affichage des arguments avec shift :"
+count=1
+while [ $# -gt 0 ]; do
+    echo "Argument $count: $1"
+    shift
+    ((count++))
 done
