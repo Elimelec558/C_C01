a6a0315002da21579cb465ad6d7e4e04b6dff9f4
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-17 17:01:13 +0000

    fsf

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-read -a arguments 
-for arg in "${arguments[@]}"; do
+
+# Vérifier s'il y a des arguments
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 argument1 argument2 ..."
+    exit 1
+fi
+
+# Afficher chaque argument sur une nouvelle ligne
+echo "Affichage des arguments :"
+for arg in "$@"; do
     echo "$arg"
 done
