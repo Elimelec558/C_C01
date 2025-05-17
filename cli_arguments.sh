970fb3b5852af13cac80e1ecfa454cdd9c714bd8
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-17 07:29:40 +0000

    gg

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 echo "Entrez les arguments séparés par des espaces :"
 read -r user_input
 
-# Convertir l'entrée utilisateur en une liste d'arguments
+# Convertir l'entrée utilisateur en arguments
 set -- $user_input
 
-echo "Affichage des arguments passés au script :"
+echo "Affichage des arguments :"
+compte=1
 for arg in "$@"; do
-  echo "Argument : $arg"
+  echo "Argument $compte : $arg"
+  compte=$((compte + 1))  # Incrémentation correcte en Bash
 done
