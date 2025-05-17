cd1ae7ac4674cba0cbe172f0fc5ae5e55cf1333a
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-17 08:33:17 +0000

    he

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-echo "Entrez les arguments séparés par des espaces :"
-read -r user_input
-
-# Convertir l'entrée utilisateur en arguments
-set -- $user_input
-
 echo "Affichage des arguments :"
 compte=1
 for arg in "$@"; do
