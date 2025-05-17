17cf5d122c5d41cfc45ab53cf7da334f2b646b22
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-17 09:14:38 +0000

    gdg

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-read -a arg  
-compte=1
-for val in "${arg[@]}"; do
-  echo "Argument $compte : $val"
-  compte=$((compte + 1))  
+read -a arg
+for i in "${!arg[@]}"; do
+  echo "Argument $((i+1)) : ${arg[i]}"
 done
