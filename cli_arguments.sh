02c0e5be94e9d040be53cbfa19344f17062ac14c
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-17 15:50:29 +0000

    sg

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # Demander à l'utilisateur d'entrer plusieurs mots ou phrases
-echo "Entrez plusieurs mots ou phrases, puis appuyez sur Entrée :"
-read -a arguments  # Stocke les entrées dans un tableau
-
-# Afficher chaque élément ligne par ligne
-echo "Affichage des arguments :"
+read -a arguments 
 for arg in "${arguments[@]}"; do
     echo "$arg"
 done
