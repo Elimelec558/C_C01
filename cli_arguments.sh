5bc3205217911b410e87995c30c1036dc1dcb938
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-17 18:55:24 +0000

    he

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-# Vérifier s'il y a des arguments
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 argument1 argument2 ..."
-    exit 1
-fi
-
-# Afficher chaque argument sur une nouvelle ligne
-echo "Affichage des arguments :"
-for arg in "$@"; do
-    echo "$arg"
+#Scrip acceptant un ou plusieurs arguments et les affichants ligne par ligne
+read -a arg
+for val in "${arg[@]}"; do
+echo "Argument: $val"
 done
