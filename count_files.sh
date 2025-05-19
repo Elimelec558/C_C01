c2e3d75a3a4d7064bdbf798f6e66fbb21fe3cc7f
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-19 08:58:54 +0000

    he

--- count_files.sh
+++ count_files.sh
@@ -2,7 +2,8 @@
 #Scrip comptant le nombre de fichiers dans un repertoire
 read rep
 if [ -d "$rep" ]; then
-echo " Le dossier $rep contient $(ls "$rep" | wc -l) fichier"
+echo " Le dossier $rep contient $(ls -l "$rep" | grep "^-" | wc -l
+) fichier"
 else
 echo "Erreur: $rep n'est pas un repertoir"
 fi
