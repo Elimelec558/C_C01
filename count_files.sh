3fcea4f7e225dcfe025442b869676d78c0f1084a
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-19 08:39:15 +0000

    je

--- count_files.sh
+++ count_files.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #Scrip comptant le nombre de fichiers dans un repertoire
 read rep
-nb_fichiers=$(ls "$rep" | wc -l)
-echo " Le dossier $rep contient $nb_fichiers fichier"
+echo " Le dossier $rep contient $(ls "$rep" | wc -l) fichier"
