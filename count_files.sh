2a11a9bb66bcf11fcc8946ba50f40e7d3e32c484
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-19 15:14:27 +0000

    gs

--- count_files.sh
+++ count_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#Scrip comptant le nombre de fichiers dans un repertoire
+#Scrip comptant le nombre de fichiers dans un repertoire.
 read repertoire
 if [ -d "$repertoire" ]; then
 echo "Le dossier $repertoire contient $(ls "$repertoire"|wc -l) fichiers."
+else
+echo "Erreur: Ce repertoire n'exist pas."
 fi
