8a5136cdb0156b54aef092c61008e7470c8def0f
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-19 14:36:54 +0000

    hd

--- count_files.sh
+++ count_files.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #Scrip comptant le nombre de fichiers dans un repertoire
 read repertoire
-echo " Le dossier $reperetoire contient $(ls -l "$repertoire" | wc -l
-) fichiers."
+echo "Le dossier $repertoire contient $(ls "$repertoire"|wc -l) fichiers."
