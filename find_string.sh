9beab21b31ffbd3c9269428d9f71b82974495267
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-20 06:28:01 +0000

    yeh

--- find_string.sh
+++ find_string.sh
@@ -2,8 +2,10 @@
 # Script recherchant une chaîne de caractères dans un fichier.
 read fichier
 read chaine
+if [ -f "$fichier" ]; then
 if grep "$chaine" "$fichier"; then 
 echo "La chaîne $chaine a été trouvée dans $fichier."
 else
 echo "La chaîne $chaine n'a pas été trouvée dans $fichier."
+fi
 fi
