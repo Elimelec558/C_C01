9a52ac461e87a8fd06bbc7b93e53aa8708525179
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-20 07:21:15 +0000

    dg

--- find_string.sh
+++ find_string.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Script recherchant une chaîne de caractères dans un fichier.
-echo "Nom du fichier :"
 read fichier
-echo "Chaîne à rechercher :"
 read chaine
 grep "$chaine" "$fichier"
