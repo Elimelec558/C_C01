4a831e4925c5e5c884eb38711a82a323cf5d9f2e
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-20 06:11:46 +0000

    ddj

--- find_string.sh
+++ find_string.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 # Script recherchant une chaîne de caractères dans un fichier.
+read fichier
+read chaine
 
-read -p "Entrez le nom du fichier : " fichier
-read -p "Entrez la chaîne à rechercher : " chaine
-
-if [ -f "$fichier" ]; then
-    echo "Le fichier $fichier existe bien."
-    if grep -q "$chaine" "$fichier"; then
-        echo "La chaîne \"$chaine\" a été trouvée dans $fichier."
-    else
-        echo "La chaîne \"$chaine\" n'a pas été trouvée dans $fichier."
-    fi
-else
-    echo "Erreur : le fichier \"$fichier\" n'existe pas."
-    exit 1
-fi
+[ -f "$fichier" ] && grep -q "$chaine" "$fichier" && echo "Trouvé !" || echo "Non trouvé."
