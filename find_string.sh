b20c4901ac91018be6a4b19b0715b60d2feb6ba4
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-20 07:04:38 +0000

    dhd

--- find_string.sh
+++ find_string.sh
@@ -3,7 +3,7 @@
 read fichier
 read chaine
 if [ -f "$fichier" ]; then
-if grep "$chaine" "$fichier"; then 
+if grep -q "$chaine" "$fichier"; then 
 echo "La chaîne '$chaine' a été trouvée dans $fichier."
 else
 echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
