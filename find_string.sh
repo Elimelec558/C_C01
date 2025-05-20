b920a2e2ba7c15c4e9f15c4f6730580fcda441bf
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-20 06:39:20 +0000

    s

--- find_string.sh
+++ find_string.sh
@@ -4,9 +4,9 @@ read fichier
 read chaine
 if [ -f "$fichier" ]; then
 if grep "$chaine" "$fichier"; then 
-echo "La chaîne $chaine a été trouvée dans $fichier."
+echo "La chaîne '$chaine' a été trouvée dans $fichier."
 else
-echo "La chaîne $chaine n'a pas été trouvée dans $fichier."
+echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
 fi
 else
 echo "Erreur."
